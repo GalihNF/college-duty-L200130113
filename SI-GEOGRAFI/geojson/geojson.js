25ba1ac3e6074f41b945a9c13e805799a79ebb4d
agusmakmun <summon.agus@gmail.com>
2016-05-12 02:18:09 +0700

    Add GeoJSON Task

---
+++ SI-GEOGRAFI/geojson/geojson.js
@@ -0,0 +1,29 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -7.52757,
+            110.82853
+          ],
+          [
+            -7.51941,
+            110.75541
+          ],
+          [
+            -7.56399,
+            110.70047
+          ],
+          [
+            -7.59190,
+            110.78218
+          ]
+        ]
+      }
+    }
+  ]
+}
